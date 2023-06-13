6c91e226ec6c1da69a7a11fe7aa0872eac6bcee7
yonatan afewerk <yonatanafewerk@gmail.com>
2023-06-13 11:08:34 +0300

    Code Editor start date june 13

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CodeEditorWindow from "./Components/CodeEditorWindow";
+
+function App() {
+    return (
+        <>
+            <h5 className="blue">Making a code editor :(</h5>
+            <CodeEditorWindow></CodeEditorWindow>
+        </>
+    );
+}
+
+export default App;
---
+++ src/Components/CodeEditorWindow.jsx
@@ -0,0 +1,39 @@
+import Editor from "@monaco-editor/react";
+
+const CodeEditorWindow = () => {
+    function getCode() {
+        return [
+            "// Oh, prepare yourself for the most hilarious joke you've ever heard!",
+            "",
+            "const jokeSetup = () => {",
+            '  const hilariousVariable = "Why did the chicken cross the road?"; // Wow, such a unique and original question!',
+            "  return hilariousVariable;",
+            "};",
+            "",
+            "const jokePunchline = (setup) => {",
+            "  const kneeSlapperFunction = (setup) => {",
+            '    const response = setup + " To get to the other side, of course!"; // Ah, the epitome of humor!',
+            "    return response;",
+            "  };",
+            "",
+            "  return kneeSlapperFunction(setup);",
+            "};",
+            "",
+            "const joke = jokePunchline(jokeSetup());",
+            "console.log(joke); // Brace yourself for an outburst of laughter!",
+        ].join("\n");
+    }
+
+    return (
+        <div className="editor">
+            <Editor
+                height="45vh"
+                width={`100%`}
+                value={getCode()}
+                language="javascript"
+                defaultValue="// some comment"
+            />
+        </div>
+    );
+};
+export default CodeEditorWindow;
